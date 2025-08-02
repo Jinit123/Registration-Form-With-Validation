8f9a70b8ce058a7808577b9914fe698268a0a871
Jinit Mewada <jinitmewada41@gmail.com>
2025-08-02 18:42:23 +0530

    Done

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -5,7 +5,7 @@ const Dashboard = () => {
     return (
         <div>
             <img src={logo} alt="" className='w-200 h-200' />
-            <h1 className='text-red-500'>Welcome to bEdge</h1>
+            <h1 className='text-red-500 text-lg'>Welcome to bEdge</h1>
         </div>
     )
 }
